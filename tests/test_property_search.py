cba1d7b1f02326fe292ef047bfc15f22639d3e41
Lazar <sc2lazar@gmail.com>
2025-06-17 14:19:08 +0200

    Updated: added two more tests that test pagination. Changed: isinstance for all tests was 'list' changed to 'dict', because with pagination 'list' was not adequte.

--- tests/test_property_search.py
+++ tests/test_property_search.py
@@ -9,42 +9,55 @@ class TestPropertySearch:
     def test_search_by_property_type(self):
         response = requests.get(TestPropertySearch.URL, params={"property_type": "kuća"})
         assert response.status_code == 200
-        assert isinstance(response.json(), list)
+        assert isinstance(response.json(), dict)
 
     def test_search_by_min_sq_footage(self):
         response = requests.get(TestPropertySearch.URL, params={"min_sq_footage": 50})
         assert response.status_code == 200
-        assert isinstance(response.json(), list)
+        assert isinstance(response.json(), dict)
 
     def test_search_by_max_sq_footage(self):
         response = requests.get(TestPropertySearch.URL, params={"max_sq_footage": 100})
         assert response.status_code == 200
-        assert isinstance(response.json(), list)
+        assert isinstance(response.json(), dict)
 
     def test_search_by_parking(self):
         response = requests.get(TestPropertySearch.URL, params={"parking": "true"})
         assert response.status_code == 200
-        assert isinstance(response.json(), list)
+        assert isinstance(response.json(), dict)
 
     def test_search_by_state(self):
         response = requests.get(TestPropertySearch.URL, params={"state": "Beograd"})
         assert response.status_code == 200
-        assert isinstance(response.json(), list)
+        assert isinstance(response.json(), dict)
 
     def test_search_by_estate_type(self):
-        response = requests.get(TestPropertySearch.URL, params={"estate_type": 1})
+        response = requests.get(TestPropertySearch.URL, params={"estate_type": "stan"})
         assert response.status_code == 200
-        assert isinstance(response.json(), list)
+        assert isinstance(response.json(), dict)
 
     def test_combined_search(self):
         query = {
-            "property_type": "kuća",
-            "min_sq_footage": 80,
-            "max_sq_footage": 200,
-            "parking": "true",
-            "state": "Beograd",
-            "estate_type": 2
-        }
+                    "property_type": "kuća",
+                    "min_sq_footage": 80,
+                    "max_sq_footage": 200,
+                    "parking": "true",
+                    "state": "Beograd",
+                    "estate_type": "kuća"
+                }
         response = requests.get(TestPropertySearch.URL, params=query)
         assert response.status_code == 200
-        assert isinstance(response.json(), list)
+        assert isinstance(response.json(), dict)
+
+    def test_pagination(self):
+        response = requests.get(TestPropertySearch.URL, params={"min_sq_footage": 70})
+        json_data = response.json()
+        assert "next_url" in json_data
+        next_url = json_data["next_url"]
+        response = requests.get(f"http://127.0.0.1:5000/{next_url}", params={"min_sq_footage": 70})
+        assert response.status_code == 200
+        
+    def test_page_wrong_type(self):
+        response = requests.get(TestPropertySearch.URL, params={"min_sq_footage": 70, "page": "abc"})
+        #flask silently fails and translates error into page=1, because we provided with the default value
+        assert response.status_code == 200
