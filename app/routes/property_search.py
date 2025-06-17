e1dbb4d562c74e243639c20f6bdb7ef4870ff6eb
Lazar <sc2lazar@gmail.com>
2025-06-17 14:01:05 +0200

    Updated: added pagination to the property search api, set up two return properties representing next page and previous page

--- app/routes/property_search.py
+++ app/routes/property_search.py
@@ -1,4 +1,4 @@
-from flask import jsonify, request, flash
+from flask import jsonify, request, flash, url_for
 from app import app, db
 import sqlalchemy as sa
 from app.models import Building, EstateType, CityPart, City
@@ -13,6 +13,8 @@ def init_property_search(bp_property_search):
         parking = request.args.get("parking", type=str)
         state = request.args.get("state", type=str)
         estate_type = request.args.get("estate_type", type=str)
+
+        page = request.args.get("page", default=1, type=int)
 
         query = db.session.query(Building).join(Building.city_part).join(CityPart.city).join(Building.estate_type)
 
@@ -36,6 +38,22 @@ def init_property_search(bp_property_search):
 
         if estate_type is not None:
             query = query.filter(Building.estate_type.has(EstateType.name.ilike(estate_type)))
+        
+        paginated_query = db.paginate(  query,
+                                        page = page,
+                                        per_page = app.config["PROPERTY_PER_PAGE"],
+                                        )
+        next_page = paginated_query.has_next
+        prev_page = paginated_query.has_prev
+        if next_page:
+            next_url = url_for(f"property_search.property_search", page=paginated_query.next_num)
+        elif prev_page:
+            next_url = url_for(f"property_search.property_search", page=paginated_query.prev_num)
 
-        results = query.all()
-        return jsonify([b.to_dict() for b in results]), 200
+        results = paginated_query
+        return jsonify({
+                        "results": [b.to_dict() for b in results],
+                        "next_page": next_page,
+                        "previous_page": prev_page,
+                        "next_url": next_url,
+                        }), 200
